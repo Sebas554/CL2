fb11a2c8a34e04565902fdba43d98d751f818e58
sebas554 <rockychongo21@gmail.com>
2024-06-23 01:29:49 -0500

    segundo

--- src/main/java/controlador/ControladorUsuario.java
+++ src/main/java/controlador/ControladorUsuario.java
@@ -1,5 +1,55 @@
 package controlador;
 
-public class ControladorUsuario {
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.ClassUsuarioimp;
+import model.TblUsuariocl2;
+
+/**
+ * Servlet implementation class ControladorUsuario
+ */
+@WebServlet("/login")
+public class ControladorUsuario extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ControladorUsuario() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.getRequestDispatcher("FormLogin.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		ClassUsuarioimp crud = new ClassUsuarioimp();
+
+        String usuario = request.getParameter("usuario");
+        String password = request.getParameter("password");
+
+        TblUsuariocl2 user = crud.validarUsuario(usuario, password);
+        if(user !=null){
+            request.getRequestDispatcher("index.jsp").forward(request, response);
+        }else{
+            request.setAttribute("error", "Usuario o contraseña incorrectos");
+            request.getRequestDispatcher("FormLogin.jsp").forward(request, response);
+        }
+	}
 
 }
